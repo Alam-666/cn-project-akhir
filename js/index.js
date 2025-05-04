51a7a277888fba8ea2fa3fb7522418e824388aa2
Alam-666 <dennyalamsyah80@gmail.com>
2025-05-04 16:03:09 +0700

    minggu 04/05/2024

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
+//SPLIDE JS
+var main = new Splide( '#main-slider', {
+    type       : 'fade',
+    heightRatio: 0.5,
+    pagination : false,
+    arrows     : false,
+    cover      : true,
+  } );
+  
+  var thumbnails = new Splide( '#thumbnail-slider', {
+    rewind          : true,
+    fixedWidth      : 140,
+    fixedHeight     : 76,
+    isNavigation    : true,
+    gap             : 10,
+    focus           : 'center',
+    pagination      : false,
+    cover           : true,
+    dragMinThreshold: {
+      mouse: 4,
+      touch: 10,
+    },
+    breakpoints : {
+      100: {
+        fixedWidth  : 66,
+        fixedHeight : 40,
+      },
+    },
+  } );
+  
+  main.sync( thumbnails );
+  main.mount();
+  thumbnails.mount();
+
+
+
 var dataMakanan = [
     'pancong',
     'kue kukus',
@@ -88,7 +124,7 @@ if(nilaiUjian>=75 && nilaiAbsensi>= 75){
 }
 
 //Ternery Operator
-const kelulusan = nilaiUjian >= 75 && nilaiAbsensi  >= 75? 'EXELLENT' : (nilaiUjian <75 && nilaiAbsensi>=)||(nilaiAbsensi<75 && nilaiUjian >=75)? 'good' : 'bad'
+const kelulusan = nilaiUjian >= 75 && nilaiAbsensi  >= 75? 'EXELLENT' : (nilaiUjian <75 && nilaiAbsensi >= 75)||(nilaiAbsensi<75 && nilaiUjian >=75)? 'good' : 'bad'
 console.log(kelulusan)
 
 // ARRAY METHODS(KONSEP / FUNGSI ARRAY)
@@ -101,63 +137,66 @@ console.log(dataKopi[5])
 const dataGabung = dataKopi.concat(data)
 console.table(dataGabung)
 
-//looping
-for(let index =0; index < dataGabung.length; index+++) {
-    console.log('DATA KE -' + index+ ':' +dataGabung[index])
-}
+// //looping
+// for(let index =0; index < dataGabung.length; index+++) {
+//     console.log('DATA KE -' + index+ ':' +dataGabung[index])
+// }
 
-const user = {
-    fullName : 'Ray'
-    email: 'Reynaldo@gmaik.co'
-    age : '25'
-}
-letdata user=''
-for(let data in user) {
-    dataUser+=user[data]+ ''
-}
-console.log dataUser
+// const user = {
+//     fullName : 'Ray'
+//     email: 'Reynaldo@gmaik.co'
+//     age : '25'
+// }
+// letdata user=''
+// for(let data in user) {
+//     dataUser+=user[data]+ ''
+// }
+// console.log dataUser
 
-const user =
-{
-    "id": 7,
-    "email": "michael.lawson@reqres.in",
-    "first_name": "Michael",
-    "last_name": "Lawson",
-    "avatar": "https://reqres.in/img/faces/7-image.jpg"
-},
+// const user =
+// {
+//     "id": 7,
+//     "email": "michael.lawson@reqres.in",
+//     "first_name": "Michael",
+//     "last_name": "Lawson",
+//     "avatar": "https://reqres.in/img/faces/7-image.jpg"
+// },
 
-{
-    "id":8,
-    "email": "lindsay ferguson@reqres.in",
-    "first_name": "Lindsay",
-    "last_name": "Ferguson",
-    "avatar": "https://reqres.in/img/faces/8-image.jpg"
-},
-{
-    "id": 9,
-    "email": "tobias.funke@reqres.in",
-    "first_name": "Tobias",
-    "last_name": "Funke",
-    "avatar": "https://reqres.in/img/faces/9-image.jpg"
-},
-{
-    "id": 10,
-    "email": "byron.fields@reqres.in",
-    "first_name": "Byron",
-    "last_name": "Fields",
-    "avatar": "https://reqres.in/img/faces/10-image.jpg"
-},
-{
-    "id": 11,
-    "email": "george.edwards@reqres.in",
-    "first_name": "George",
-    "last_name": "Edwards",
-    "avatar": "https://reqres.in/img/faces/11-image.jpg"
-},
-{
-    "id": 12,
-    "email": "rachel.howell@reqres.in",
-    "first_name": "Rachel",
-    "last_name": "Howell",
-    "avatar": "https://reqres.in/img/faces/12-image.jpg"
-}
+// {
+//     "id":8,
+//     "email": "lindsay ferguson@reqres.in",
+//     "first_name": "Lindsay",
+//     "last_name": "Ferguson",
+//     "avatar": "https://reqres.in/img/faces/8-image.jpg"
+// },
+
+// {
+//     "id": 9,
+//     "email": "tobias.funke@reqres.in",
+//     "first_name": "Tobias",
+//     "last_name": "Funke",
+//     "avatar": "https://reqres.in/img/faces/9-image.jpg"
+// },
+
+// {
+//     "id": 10,
+//     "email": "byron.fields@reqres.in",
+//     "first_name": "Byron",
+//     "last_name": "Fields",
+//     "avatar": "https://reqres.in/img/faces/10-image.jpg"
+// },
+
+// {
+//     "id": 11,
+//     "email": "george.edwards@reqres.in",
+//     "first_name": "George",
+//     "last_name": "Edwards",
+//     "avatar": "https://reqres.in/img/faces/11-image.jpg"
+// },
+// {
+//     "id": 12,
+//     "email": "rachel.howell@reqres.in",
+//     "first_name": "Rachel",
+//     "last_name": "Howell",
+//     "avatar": "https://reqres.in/img/faces/12-image.jpg"
+// }
